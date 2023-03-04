4bac629ce306a8be5d653f3bdccd14213f902d55
viktorija-verebes <viki.verebes90@gmail.com>
2023-03-04 20:41:18 +0100

    mobile navigation

--- js/main.js
+++ js/main.js
@@ -9,3 +9,26 @@ modalBtn.addEventListener('click', function() {
 closeBtn.addEventListener('click', function() {
     modal.classList.remove("open-modal");
 });
+
+ const navToggle = document.querySelector('.nav-toggle');
+ const menu = document.querySelector('.dropdownMenu');
+ const list = document.querySelector('.nav-menu__left');
+ const closeNav = document.querySelector('.close-nav');
+
+ navToggle.addEventListener('click', function() {
+    console.log("ghhdh");
+    if(menu.classList.contains('show-links') && list.classList.contains('show-list')){
+        menu.classList.remove('show-links');
+        list.classList.remove('show-list');
+    } else {
+        menu.classList.add('show-links');
+        list.classList.add('show-list');
+    }
+ });
+
+ closeNav.addEventListener('click', function() {
+    if(menu.classList.contains('show-links') && list.classList.contains('show-list')){
+        menu.classList.remove('show-links');
+        list.classList.remove('show-list');
+    }
+ });
